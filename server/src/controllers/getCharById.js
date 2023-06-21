e29d035a02b4c1b625f7e086a0892afd1d0128cb
Jaasiel López <jglm88_lp@hotmail.com>
2023-06-21 17:38:38 -0600

    server async await

--- server/src/controllers/getCharById.js
+++ server/src/controllers/getCharById.js
@@ -28,31 +28,62 @@
 
 // module.exports = getCharById;
 
-//!			-----** EXPRESS **-----
+//!			-----*** EXPRESS && PROMISES ***-----
+// const URL = "https://rickandmortyapi.com/api/character/";
+// const axios = require("axios");
+// const { response } = require("express");
+
+// const getCharById = (req, res) => {
+// 	const { id } = req.params;
+// 	axios(`${URL}/${id}`)
+// 		.then((response) => response.data)
+// 		.then(({ status, name, species, origin, image, gender, id }) => {
+// 			if (name) {
+// 				const character = {
+// 					id,
+// 					name,
+// 					species,
+// 					origin,
+// 					image,
+// 					gender,
+// 					status,
+// 				};
+// 				return res.status(200).json(character);
+// 			}
+// 			return res.status(404).send("Not found");
+// 		})
+// 		.catch((error) => res.status(500).send(error.message));
+// };
+
+// module.exports = { getCharById };
+
+//!			-----*** EXPRESS && Async/Await && Try/Catch ***-----
 const URL = "https://rickandmortyapi.com/api/character/";
 const axios = require("axios");
 const { response } = require("express");
 
-const getCharById = (req, res) => {
-	const { id } = req.params;
-	axios(`${URL}/${id}`)
-		.then((response) => response.data)
-		.then(({ status, name, species, origin, image, gender, id }) => {
-			if (name) {
-				const character = {
-					id,
-					name,
-					species,
-					origin,
-					image,
-					gender,
-					status,
-				};
-				return res.status(200).json(character);
-			}
-			return res.status(404).send("Not found");
-		})
-		.catch((error) => res.status(500).send(error.message));
+const getCharById = async (req, res) => {
+	try {
+		const { id } = req.params;
+		const { data } = await axios(`${URL}/${id}`);
+
+		if (!data.name) throw new Error(`ID: ${id} Not found`);
+
+		const character = {
+			id: data.id,
+			name: data.name,
+			species: data.species,
+			origin: data.origin,
+			image: data.image,
+			gender: data.gender,
+			status: data.status,
+		};
+		return res.status(200).json(character);
+	} catch (error) {
+		return error.message.includes("ID")
+			? res.status(404).send(error.message)
+			: res.status(500).send(error.message);
+	}
 };
 
 module.exports = { getCharById };
